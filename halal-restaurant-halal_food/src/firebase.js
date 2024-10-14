45148a29bf5d508c433b1932f96d10f22da1b5f4
MohammedAK9 <modydxx@gmail.com>
2024-10-14 16:10:49 -0500

    added firebase

--- halal-restaurant-halal_food/src/firebase.js
+++ halal-restaurant-halal_food/src/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDOAmOYWJi13qRYBwbfsvqkVLx_OkJyPVQ",
+  apiKey: "AIzaSyDOAmOYWJi13qRYBwbfsvqkVLx_OkJyPVQ", // my api 
   authDomain: "halalfood-fe458.firebaseapp.com",
   projectId: "halalfood-fe458",
   storageBucket: "halalfood-fe458.appspot.com",
